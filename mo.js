45bfd105dc4fe2b71c8167f9983fbb053eea3e1e
JojoDuke <jojoamankwa@gmail.com>
2021-12-04 00:48:48 +0000

    sure

---
+++ mo.js
@@ -0,0 +1,28 @@
+const Flutterwave = require('flutterwave-node-v3');
+
+const flw = new Flutterwave("FLWPUBK-ceabbd43b8f178fa6da2a6681cbe516e-X", "FLWSECK-1d731caf0259ea50d8cd3ddbd86c92dd-X");
+
+const Gh_mobilemoney =  async () =>{
+ 
+    try {
+
+        const payload = {
+            "tx_ref": "87869",
+            "amount": "580",
+            "type": "mobile_money_ghana",
+            "currency": "GHS",
+            "network": "MTN", //This is the customer's mobile money network provider (possible values: MTN, VODAFONE, TIGO)
+            "phone_number": "0240369071",
+            "email":"dukeopoku@gmail.com",
+    }
+
+       const response =  await flw.MobileMoney.ghana(payload)
+       console.log(response);
+    } catch (error) {
+        console.log(error)
+    }                            
+   
+}
+ 
+ 
+Gh_mobilemoney();
