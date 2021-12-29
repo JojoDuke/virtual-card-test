ca38c9b8e0d8553e83286d32210038801858990b
Jojo Duke <45549237+JojoDuke@users.noreply.github.com>
2021-12-29 01:06:23 +0000

    Update mo.js
    jk

--- mo.js
+++ mo.js
@@ -19,7 +19,7 @@ const Gh_mobilemoney =  async () =>{
        const response =  await flw.MobileMoney.ghana(payload)
        console.log(response);
     } catch (error) {
-        console.log(error)
+        console.log(erro)
     }                            
    
 }
